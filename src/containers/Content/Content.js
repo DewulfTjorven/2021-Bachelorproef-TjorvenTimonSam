396e01ba98b37ec0ac469e48f4664498f981c7c4
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2020-03-11 13:03:46 +0100

    Let op het `<Empty>` component. Deze kan je gebruiken indien je geen specifiek content component kan tonen

--- src/containers/Content/Content.js
+++ src/containers/Content/Content.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import Empty from "../../components/Empty/Empty";
 import style from "./Content.module.css";
-
+import { Switch, Route } from "react-router-dom";
+import { ROUTES } from "../../consts";
 
 const Content = () => {
   return (
     <section className={style.container}>
-
+      <Switch>
+        <Route exact strict path={ROUTES.home}>
+          <Empty message={"Pick a conversation"} />
+        </Route>
+      </Switch>
     </section>
   );
 };
