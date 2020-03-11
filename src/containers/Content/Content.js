1a5b4747a1416b6adf8f18f1c475dd96faf925ca
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2020-03-11 13:07:26 +0100

    ### Messages
    - Toont de messages van een group, maar deze komen niet uit de props zoals er daar nu staat. Haal de id van de huidige conversation (group) uit de route en haal hiermee de group op.

--- src/containers/Content/Content.js
+++ src/containers/Content/Content.js
@@ -3,11 +3,15 @@ import Empty from "../../components/Empty/Empty";
 import style from "./Content.module.css";
 import { Switch, Route } from "react-router-dom";
 import { ROUTES } from "../../consts";
+import Messages from "./Messages/Messages";
 
 const Content = () => {
   return (
     <section className={style.container}>
       <Switch>
+        <Route path={ROUTES.conversation.path}>
+          <Messages />
+        </Route>
         <Route exact strict path={ROUTES.home}>
           <Empty message={"Pick a conversation"} />
         </Route>
--- src/containers/Content/Messages/Messages.js
+++ src/containers/Content/Messages/Messages.js
@@ -5,36 +5,49 @@ import Form from "../../../components/Form/Form.js";
 import UnreadMessages from "../../../components/UnreadMessages/UnreadMessages.js";
 
 import style from "./Messages.module.css";
+import { useParams } from "react-router-dom";
+import { useStores } from "../../../hooks/useStores.js";
+import { useObserver } from "mobx-react-lite";
+import Empty from "../../../components/Empty/Empty.js";
 
-const Messages = ({ group }) => {
-  return (
-    <>
-      <header className={style.header}>
-        {group && (
-          <>
-            <img
-              className={style.img}
-              src={group.pic}
-              alt="Group img"
-              width="50"
-              height="50"
-            />
-            <h3 className={style.title}>{group.name}</h3>
-            <span className={style.unread}>
-              {group.users.length} members -{" "}
-              <UnreadMessages length={group.unreadLength} />
-            </span>
-          </>
-        )}
-      </header>
-      <ul className={style.list}>
-        {group.messages.map(message => (
-          <Message message={message} key={message.id} />
-        ))}
-      </ul>
-      <Form />
-    </>
-  );
+const Messages = () => {
+  const { id } = useParams();
+  const { dataStore } = useStores();
+  const group = dataStore.getGroupById(id);
+
+  return useObserver(() => {
+    if (!group) {
+      return <Empty message={"Conversation not found"} />;
+    }
+    return (
+      <>
+        <header className={style.header}>
+          {group && (
+            <>
+              <img
+                className={style.img}
+                src={group.pic}
+                alt="Group img"
+                width="50"
+                height="50"
+              />
+              <h3 className={style.title}>{group.name}</h3>
+              <span className={style.unread}>
+                {group.users.length} members -{" "}
+                <UnreadMessages length={group.unreadLength} />
+              </span>
+            </>
+          )}
+        </header>
+        <ul className={style.list}>
+          {group.messages.map(message => (
+            <Message message={message} key={message.id} />
+          ))}
+        </ul>
+        <Form />
+      </>
+    );
+  });
 };
 
 export default Messages;
