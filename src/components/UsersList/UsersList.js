f8530bbb4442aef6ab9f9e340220721e2f919b7c
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2020-03-11 13:24:05 +0100

    ### UsersList
    - Wat heeft die .map() nodig?

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,16 +1,20 @@
 import React from "react";
 import User from "../User/User";
 
+import { useObserver } from "mobx-react-lite";
+import { useStores } from "../../hooks/useStores";
+
 const UsersList = () => {
-  return (
+  const { dataStore } = useStores();
+  return useObserver(() => (
     <>
       <ul>
-        {[].map(user => (
+        {dataStore.users.map(user => (
           <User user={user} key={user.id} />
         ))}
       </ul>
     </>
-  );
+  ));
 };
 
 export default UsersList;
