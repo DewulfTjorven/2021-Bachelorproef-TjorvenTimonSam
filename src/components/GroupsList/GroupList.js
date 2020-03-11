731616d1ab3e72dffecd5f114ef966dacc3566ff
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2020-03-11 13:05:32 +0100

    - Wat heeft die .map() nodig?

--- src/components/GroupsList/GroupList.js
+++ src/components/GroupsList/GroupList.js
@@ -1,17 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Group from "../Group/Group.js";
+import { useObserver } from "mobx-react-lite";
+import { useStores } from "../../hooks/useStores";
 
 const GroupsList = ({ detailRoute }) => {
-  return (
+  const { dataStore } = useStores();
+  return useObserver(() => (
     <>
       <ul className="groups">
-        {[].map(group => (
+        {dataStore.groups.map(group => (
           <Group group={group} key={group.id} detailRoute={detailRoute} />
         ))}
       </ul>
     </>
-  );
+  ));
 };
 
 GroupsList.propTypes = {
