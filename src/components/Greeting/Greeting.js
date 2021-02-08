c284b5a8db1a9d755b96528512e9b22bb886c88d
Timon Wullus <timon.wullus@student.howest.be>
2021-02-08 15:34:34 +0100

    help button

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Help } from "./help.svg";
+import { ROUTES } from "../../consts";
 
 
 import style from "./Greeting.module.css";
@@ -12,6 +15,19 @@ const Greeting = () => {
         <h3 className={style.title__small}>Goeiedag</h3>
         <p>Wat wilt u vandaag gaan doen?</p>
       </section>
+
+      <section>
+        {ROUTES === ROUTES.bewonerinterface ? 
+        
+        <Link to={ROUTES.hulpvideos}>
+          <button className={style.button}>
+            <Help />
+            <p>Ik heb hulp nodig</p>
+          </button>
+        </Link> : <p></p>}
+              
+      </section>
+      
       <section className={style.contentGreeting}>
         <h3 className={style.clock}>14:22</h3>
       </section>
